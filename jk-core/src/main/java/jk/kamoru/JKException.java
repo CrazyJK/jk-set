fa527252d1323280ccb0103ca7da53f04e339afe
kamoru-mac <Crazy.4.JK@gmail.com>
2016-05-10 21:38:25 +0900

    add constractor for JDK7

--- jk-core/src/main/java/jk/kamoru/JKException.java
+++ jk-core/src/main/java/jk/kamoru/JKException.java
@@ -9,12 +9,12 @@ public abstract class JKException extends RuntimeException {
 
 	private static final long serialVersionUID = JK.SERIAL_VERSION_UID;
 
-/*	sinse JDK 1.7
-	public KamoruException(String message, Throwable cause,
+/*	sinse JDK 1.7 */
+	public JKException(String message, Throwable cause,
 			boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
 	}
-*/
+
 	public JKException(String message, Throwable cause) {
 		super(message, cause);
 	}
